6510b4a2aa96bc5a90f885f022ae7c325e20ac5b
ciceron166 <78279847+ciceron166@users.noreply.github.com>
2024-04-16 18:40:23 +0200

    Add files via upload

---
+++ BusinessLayer/BusinessProduct.cs
@@ -0,0 +1,44 @@
+﻿using DataLayer;
+using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class BusinessProduct : IBusinessProduct
+    {
+        private readonly IProductRepository _productRepository;
+
+        public BusinessProduct(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<List<Product>> GetAll()
+        {
+            var products = await _productRepository.GetAll();
+            if ( products.Count > 0)
+            {
+                return products;
+            }
+            return new List<Product>();
+
+        }
+
+        public List<Product> GetInterval(decimal price1, decimal price2)
+        {
+            return new List<Product>();
+        }
+
+        public bool Insert(Product product)
+        {
+            if (string.IsNullOrEmpty(product.Name) || string.IsNullOrEmpty(product.Description)
+                || product.Price == 0)
+                return false;
+           return _productRepository.Insert(product);
+        }
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer;
+using DataLayer.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IBusinessProduct businessProduct;
+
+        public ProductsController(IBusinessProduct businessProduct)
+        {
+            this.businessProduct = businessProduct;
+        }
+
+
+
+        // GET: api/<ProductsController>
+        [HttpGet("getall")]
+        public async Task<List<Product>> Get()
+        {
+            return await businessProduct.GetAll();
+        }
+
+        // interval
+        [HttpGet("{minPrice}/{maxPrice}")]
+        public List<Product> GetInterval(decimal minPrice, decimal maxPrice)
+        {
+            if (minPrice > maxPrice) return new List<Product>();
+            return businessProduct.GetInterval(minPrice, maxPrice);
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Product product)
+        {
+            return Ok(businessProduct.Insert(product));
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
